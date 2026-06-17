d7cab2b5192eb161f5491c8613f4c84bc31d8251
parvgupta <gupta.parveen.tech@gmail.com>
2026-06-17 12:42:54 +0530

    changes in main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_resource_group" "rgs" {
-    name = "dev-rg2231"
+    name = "dev-rg22312"
     location  = "centralindia"
 }
 
 resource "azurerm_storage_account" "stacc" {
     depends_on = [azurerm_resource_group.rgs]
-    name = "storaccparv3321"
+    name = "storaccparv33212"
     location = "centralindia"
-    resource_group_name = "dev-rg2231"
+    resource_group_name = "dev-rg22312"
     account_tier = "Standard"
     account_replication_type = "LRS"
 }
